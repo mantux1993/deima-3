88b971380695ee39e1d607b950710455debddbae
mantux1993 <wizha123@gmail.com>
2026-02-01 10:46:22 +0200

    config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Ar būsi mano Valentine? 💝",
+    pageTitle: "labai svarbus klausimas 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
